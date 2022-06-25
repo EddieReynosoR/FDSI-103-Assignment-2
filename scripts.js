cdbe829f15f890ece687eba4fac59727448c3678
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-06-25 11:17:22 -0700

    New code

--- scripts.js
+++ scripts.js
@@ -17,11 +17,13 @@ console.log("Final Price with IVA 10%: " + (Total));
 
 
 document.write(`
-    <h1> Receipt </h1>
-    <p> <b>Product name: </b> ${ProductName} </p>
-    <p> <b>Product price: </b> ${ProductPrice} </p>
-    <p> <b>Quantity: </b> ${ProductQty} </p>
-    <p> <b>Total price: </b> ${TotalPrice} </p>
-    <p> <b>Tax (10%): </b> ${taxPrice} </p>
-    <p> <b>Total: </b> ${Total};
+    <div style="text-align: center; border: solid; width: 350px;">
+        <h1> Receipt </h1>
+        <p> <b>Product name: </b> ${ProductName} </p>
+        <p> <b>Product price: </b> $${ProductPrice} </p>
+        <p> <b>Quantity: </b> ${ProductQty} </p>
+        <p> <b>Total price: </b> $${TotalPrice} </p>
+        <p> <b>Tax (10%): </b> $${taxPrice} </p>
+        <p> <b>Total: </b> $${Total}
+    </div>
     `);
